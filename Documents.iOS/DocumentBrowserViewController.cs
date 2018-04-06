18b64e2a9465c37d18a334a2baf94423921518c5
Shaun Hevey <shaun@hevey.co>
2018-04-07 01:27:57 +1000

    initial commit for unarchive

--- Documents.iOS/DocumentBrowserViewController.cs
+++ Documents.iOS/DocumentBrowserViewController.cs
@@ -2,7 +2,9 @@ using Foundation;
 using System;
 using System.IO;
 using UIKit;
-
+using System.IO.Compression;
+using System.Linq;
+using Documents.iOS.Managers;
 namespace Documents.iOS
 {
     public partial class DocumentBrowserViewController : UIDocumentBrowserViewController
@@ -21,10 +23,13 @@ namespace Documents.iOS
             AllowsPickingMultipleItems = true;
 
             var renameWithExt = new UIDocumentBrowserAction("com.glennhevey.rename-with-extension", "Full Rename", UIDocumentBrowserActionAvailability.Menu, RenameWithExtensionAction);
+            var unarchiveExt = new UIDocumentBrowserAction("com.glennhevey.unarchive", "Unarchive", UIDocumentBrowserActionAvailability.Menu, Unarchive);
 
             renameWithExt.SupportedContentTypes = new string[] { "public.item" };
+            unarchiveExt.SupportedContentTypes = new string[] { "public.archive" };
 
-            CustomActions = new UIDocumentBrowserAction[] { renameWithExt };
+
+            CustomActions = new UIDocumentBrowserAction[] { renameWithExt, unarchiveExt };
 
             var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             var directoryname = Path.Combine(documents, "Downloads");
@@ -55,6 +60,111 @@ namespace Documents.iOS
 
 
         }
+
+        void Unarchive(NSUrl[] obj)
+        {
+            var am = new ArchiveManager();
+            var path = obj[0].Path;
+            //Create Alert
+            var unarchiveLocationController = UIAlertController.Create("Unarchive", "Unarchive file to folder", UIAlertControllerStyle.ActionSheet);
+
+            unarchiveLocationController.AddAction(UIAlertAction.Create("Current Folder", UIAlertActionStyle.Default, (Action) => {
+
+                am.UnarchiveFile(path, UnarchiveLocationEnum.CurrentDirectory);
+            }));
+
+            unarchiveLocationController.AddAction(UIAlertAction.Create($"Sub Folder ({Path.GetFileNameWithoutExtension(path)})", UIAlertActionStyle.Default, (Action) =>
+            {
+                var location = am.DetermineExtractLocation(path, UnarchiveLocationEnum.SubDirectoryWithArchiveName, "");
+
+                if (Directory.Exists(location))
+                {
+                    var okAlertController = UIAlertController.Create("Folder Exists", $"{Path.GetFileNameWithoutExtension(path)} would you like to delete it?", UIAlertControllerStyle.Alert);
+
+                    //Add Action
+                    okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (sender) => {
+                        Directory.Delete(location, true);
+                        am.UnarchiveFile(path, UnarchiveLocationEnum.SubDirectoryWithArchiveName);
+
+                    }));
+
+                    okAlertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+
+                    // Present Alert
+                    PresentViewController(okAlertController, true, null);
+
+                }
+                else
+                {
+                    am.UnarchiveFile(path, UnarchiveLocationEnum.SubDirectoryWithArchiveName);
+                }
+            }));
+            unarchiveLocationController.AddAction(UIAlertAction.Create("Sub Folder (Custom)", UIAlertActionStyle.Default, (Action) => {
+                var folderName = "";
+                var okAlertController = UIAlertController.Create("Folder Name", $"Please enter the folder to use?", UIAlertControllerStyle.Alert);
+                okAlertController.AddTextField(textField => {
+                    // If you need to customize the text field, you can do so here.
+                });
+                //Add Action
+                okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (sender) => {
+                    folderName = okAlertController.TextFields.First().Text;
+                    if (folderName != "")
+                    {
+
+
+                        var location = am.DetermineExtractLocation(path, UnarchiveLocationEnum.SubDirectoryWithName, folderName);
+
+                        if (Directory.Exists(location))
+                        {
+                            var oakAlertController = UIAlertController.Create("Folder Exists", $"{folderName} would you like to delete it?", UIAlertControllerStyle.Alert);
+
+                            //Add Action
+                            oakAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (saender) =>
+                            {
+                                Directory.Delete(location, true);
+                                am.UnarchiveFile(path, UnarchiveLocationEnum.SubDirectoryWithName, folderName);
+
+                            }));
+
+                            oakAlertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+
+                            // Present Alert
+                            PresentViewController(oakAlertController, true, null);
+
+                        }
+                        else
+                        {
+                            am.UnarchiveFile(path, UnarchiveLocationEnum.SubDirectoryWithName, folderName);
+                        }
+                    }
+                    else
+                    {
+                        var oakAlertController = UIAlertController.Create("Error", $"Folder name can't be blank.", UIAlertControllerStyle.Alert);
+
+                        //Add Action
+                        oakAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+
+                        // Present Alert
+                        PresentViewController(oakAlertController, true, null);
+                    }
+
+                }));
+
+                okAlertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+
+                // Present Alert
+                PresentViewController(okAlertController, true, null);
+
+
+
+            }));
+            unarchiveLocationController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+
+            // Present Alert
+            PresentViewController(unarchiveLocationController, true, null);
+        }
+
+
 
     }
 }
---
+++ Documents.iOS/Managers/ArchiveManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Documents.iOS.Enums;
+using System.IO;
+using System.IO.Compression;
+
+namespace Documents.iOS.Managers
+{
+    public class ArchiveManager
+    {
+        public ArchiveManager()
+        {
+        }
+
+        public void UnarchiveFile(string filePath, UnarchiveLocationEnum location, string folderToSave = "")
+        {
+            var archiveType = DetermineArchiveType(filePath);
+            var extractLocation = DetermineExtractLocation(filePath, location, folderToSave);
+
+            CreateExtractLocation(location, extractLocation);
+
+            switch (archiveType)
+            {
+                case ArchiveTypeEnum.Zip:
+                    UnarchiveZip(filePath, extractLocation);
+                    break;
+            }
+
+        }
+
+        private void UnarchiveZip(string filePath, string extractLocation)
+        {
+            ZipFile.ExtractToDirectory(filePath, extractLocation);
+        }
+
+        public string DetermineExtractLocation(string filePath, UnarchiveLocationEnum location, string folderToSave)
+        {
+            var folderName = Path.GetFileNameWithoutExtension(filePath);
+            var folderPath = Path.GetDirectoryName(filePath);
+            switch (location)
+            {
+                case UnarchiveLocationEnum.CurrentDirectory:
+                    return folderPath;
+                case UnarchiveLocationEnum.SubDirectoryWithArchiveName:
+                    return Path.Combine(folderPath, folderName);
+                case UnarchiveLocationEnum.SubDirectoryWithName:
+                    return Path.Combine(folderPath, folderToSave);
+                default:
+                    throw new Exception("Unsupported folder location");
+            }
+        }
+
+        private void CreateExtractLocation(UnarchiveLocationEnum location, string extractLocation)
+        {
+            if (location != UnarchiveLocationEnum.CurrentDirectory)
+            {
+                if (!Directory.Exists(extractLocation))
+                {
+                    Directory.CreateDirectory(extractLocation);
+                }
+            }
+        }
+
+        private ArchiveTypeEnum DetermineArchiveType(string filePath)
+        {
+            var fileExt = System.IO.Path.GetExtension(filePath);
+            switch (fileExt.ToLower())
+            {
+                case ".zip":
+                    return ArchiveTypeEnum.Zip;
+                    break;
+                default:
+                    throw new Exception("Unsupported file type");
+            }
+        }
+    }
+}
