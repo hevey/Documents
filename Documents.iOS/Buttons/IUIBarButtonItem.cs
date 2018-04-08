93ace0f0905695f2c898bf7fb68b32fc2e1c1915
Glenn <glenn.hevey@gmail.com>
2018-04-09 06:45:07 +1000

    Added Initial File Templates. Setup initial PoC for creating a new docx document

--- Documents.iOS/Buttons/IUIBarButtonItem.cs
+++ Documents.iOS/Buttons/IUIBarButtonItem.cs
@@ -1,9 +1,11 @@
-﻿using UIKit;
+﻿using System;
+using UIKit;
 
 namespace Documents.iOS.Buttons
 {
     public interface IUIBarButtonItem
     {
         UIBarButtonItem SetUiBarButtonItem();
+        void ClickedEvent(object sender, EventArgs args);
     }
 }
--- Documents.iOS/Buttons/NewItemBarButton.cs
+++ Documents.iOS/Buttons/NewItemBarButton.cs
@@ -1,4 +1,6 @@
-﻿using UIKit;
+﻿using System;
+using System.IO;
+using UIKit;
 
 namespace Documents.iOS.Buttons
 {
@@ -6,7 +8,19 @@ namespace Documents.iOS.Buttons
     {
         public UIBarButtonItem SetUiBarButtonItem()
         {
-            return new UIBarButtonItem(UIBarButtonSystemItem.Add);
+            var button = new UIBarButtonItem(UIBarButtonSystemItem.Add);
+            button.Clicked += ClickedEvent;
+            return button;
+
+        }
+
+        public void ClickedEvent(object sender, EventArgs args)
+        {
+            var docsPath = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+            var location = Path.Combine(docsPath, "Untitled.docx");
+
+            var wordDocument = Path.Combine("TemplateFiles", "Untitled.docx");
+            File.Copy(wordDocument,location);
         }
     }
 }
