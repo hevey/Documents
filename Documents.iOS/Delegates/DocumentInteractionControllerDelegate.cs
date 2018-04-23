8da9ed707d8b67c7dec231faf776d0c04c54c2fc
Glenn <glenn.hevey@gmail.com>
2018-04-23 23:53:41 +1000

    Open Files
    Initial Code for opening files

---
+++ Documents.iOS/Delegates/DocumentInteractionControllerDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using UIKit;
+
+namespace Documents.iOS.Delegates
+{
+    public class DocumentInteractionControllerDelegate : UIDocumentInteractionControllerDelegate
+    {
+        UIViewController _controller;
+        public DocumentInteractionControllerDelegate(UIViewController controller)
+        {
+            _controller = controller;
+        }
+
+        public override UIViewController ViewControllerForPreview(UIDocumentInteractionController controller)
+        {
+            return _controller;
+        }
+
+
+        public override UIView ViewForPreview(UIDocumentInteractionController controller)
+        {
+            return _controller.View;
+        }
+
+    }
+}
