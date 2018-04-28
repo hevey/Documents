6f3546c14cea178397d02036ec0248a0e05a3316
Shaun Hevey <shaun@hevey.co>
2018-04-28 23:01:38 +1000

    adding 7z type as explicit content type

--- Documents.iOS/Actions/UnarchiveAction.cs
+++ Documents.iOS/Actions/UnarchiveAction.cs
@@ -22,62 +22,67 @@ namespace Documents.iOS.Actions
         public UIDocumentBrowserAction SetupAction()
         {
             var unarchiveExt = new UIDocumentBrowserAction("com.glennhevey.unarchive", "Extract", UIDocumentBrowserActionAvailability.Menu, Action);
-            unarchiveExt.SupportedContentTypes = new string[] { "public.archive", "public.rar" };
+            unarchiveExt.SupportedContentTypes = new string[] { "public.archive", "public.rar", "public.7z" };
             return unarchiveExt;
         }
 
         public void Action(NSUrl[] obj)
         {
+            foreach (var url in obj)
+            {
+                var am = new ArchiveManager();
+                var path = url.Path;
+                //Create Alert
+                var unarchiveLocationController = UIAlertController.Create("Extract", "Extract file to folder", UIAlertControllerStyle.ActionSheet);
 
-            var am = new ArchiveManager();
-            var path = obj[0].Path;
-            //Create Alert
-            var unarchiveLocationController = UIAlertController.Create("Extract", "Extract file to folder", UIAlertControllerStyle.ActionSheet);
-
-            unarchiveLocationController.AddAction(UIAlertAction.Create("Current Folder", UIAlertActionStyle.Default, (actionparam) => Unarchive(actionparam, UnarchiveLocationEnum.CurrentDirectory, path)));
-            unarchiveLocationController.AddAction(UIAlertAction.Create($"Sub Folder ({Path.GetFileNameWithoutExtension(path)})", UIAlertActionStyle.Default, (actionparam) => Unarchive(actionparam, UnarchiveLocationEnum.SubDirectoryWithArchiveName, path)));
-            unarchiveLocationController.AddAction(UIAlertAction.Create("Sub Folder (Custom)", UIAlertActionStyle.Default, (actionparam) => {
-                var folderName = "";
-                var okAlertController = UIAlertController.Create("Folder Name", $"Please enter the folder to use?", UIAlertControllerStyle.Alert);
-                okAlertController.AddTextField(textField => {
-                    // If you need to customize the text field, you can do so here.
-                });
-                //Add Action
-                okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (sender) => {
-                    folderName = okAlertController.TextFields.First().Text;
-                    if (folderName != "")
+                unarchiveLocationController.AddAction(UIAlertAction.Create("Current Folder", UIAlertActionStyle.Default, (actionparam) => Unarchive(actionparam, UnarchiveLocationEnum.CurrentDirectory, path)));
+                unarchiveLocationController.AddAction(UIAlertAction.Create($"Sub Folder ({Path.GetFileNameWithoutExtension(path)})", UIAlertActionStyle.Default, (actionparam) => Unarchive(actionparam, UnarchiveLocationEnum.SubDirectoryWithArchiveName, path)));
+                unarchiveLocationController.AddAction(UIAlertAction.Create("Sub Folder (Custom)", UIAlertActionStyle.Default, (actionparam) =>
+                {
+                    var folderName = "";
+                    var okAlertController = UIAlertController.Create("Folder Name", $"Please enter the folder to use?", UIAlertControllerStyle.Alert);
+                    okAlertController.AddTextField(textField =>
                     {
-                        Unarchive(actionparam, UnarchiveLocationEnum.SubDirectoryWithName, path, folderName);
-                    }
-                    else
+                        // If you need to customize the text field, you can do so here.
+                    });
+                    //Add Action
+                    okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, (sender) =>
                     {
-                        var oakAlertController = UIAlertController.Create("Error", $"Folder name can't be blank.", UIAlertControllerStyle.Alert);
+                        folderName = okAlertController.TextFields.First().Text;
+                        if (folderName != "")
+                        {
+                            Unarchive(actionparam, UnarchiveLocationEnum.SubDirectoryWithName, path, folderName);
+                        }
+                        else
+                        {
+                            var oakAlertController = UIAlertController.Create("Error", $"Folder name can't be blank.", UIAlertControllerStyle.Alert);
 
-                        //Add Action
-                        oakAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                            //Add Action
+                            oakAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
 
-                        // Present Alert
-                        _view.PresentViewController(oakAlertController, true, null);
-                    }
+                            // Present Alert
+                            _view.PresentViewController(oakAlertController, true, null);
+                        }
+
+                    }));
+
+                    okAlertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+                    // Present Alert
+                    _view.PresentViewController(okAlertController, true, null);
+
+
 
                 }));
+                unarchiveLocationController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
 
-                okAlertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+                if (unarchiveLocationController.PopoverPresentationController != null)
+                {
+                    unarchiveLocationController.PopoverPresentationController.SourceView = _view.View;
+                    unarchiveLocationController.PopoverPresentationController.SourceRect = new CGRect(_view.View.Bounds.GetMidX(), _view.View.Bounds.GetMidY(), 0, 0);
+                }
                 // Present Alert
-                _view.PresentViewController(okAlertController, true, null);
-
-
-
-            }));
-            unarchiveLocationController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
-
-            if (unarchiveLocationController.PopoverPresentationController != null)
-            {
-                unarchiveLocationController.PopoverPresentationController.SourceView = _view.View;
-                unarchiveLocationController.PopoverPresentationController.SourceRect = new CGRect(_view.View.Bounds.GetMidX(), _view.View.Bounds.GetMidY(), 0, 0);
+                _view.PresentViewController(unarchiveLocationController, true, null);
             }
-            // Present Alert
-            _view.PresentViewController(unarchiveLocationController, true, null);
         }
 
         private void Unarchive(UIAlertAction action, UnarchiveLocationEnum location, string filePath, string folderToSave = "")
